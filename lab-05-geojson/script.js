f3ebd9292b78a3073d39d06e3541a54ce3b3e132
neomq <neominqi@hotmail.com>
2022-02-19 16:46:22 +0000

    updated geojson

--- lab-05-geojson/script.js
+++ lab-05-geojson/script.js
@@ -1,21 +1,10 @@
 // 1. INITIALISE THE MAP
-// Create a map of the center of Singapore
-// use an array of 2 elements to represent the lat and long
 let singapore = [1.29, 103.85];
 
 // 2. CREATE THE MAP
-// L is the leaflet object in the global scope
-// (created from the leaflet JS file)
-// L.map() creates a map object
-// and takes in one argument (the ID to place the map)
-// .setView() --> .setView(center coordinates, zoom level) 
 let mapObject = L.map('sgMap').setView(singapore, 13);
 
 // 3. SETUP THE TILE LAYERS - BOILERPLATE
-// in other words - the drawing of the map
-// copy and paste the boilerplate from leaflet
-// for different views/colors - explore mapbox - map styles
-// get own mapbox access token (below access token for demo only)
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -24,3 +13,49 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(mapObject); // Add to map object created in line 12
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// LOAD IN THE CYCLING TRACKS DATA
+async function loadCyclingPath(){
+    let response = await axios.get('data/cycle.geojson');
+    // second argument is a config object
+    let cyclingLayer = L.geoJson(response.data, {
+        // NOTE: every line in the geojson file is known as a feature - we are able to process each feature
+        onEachFeature:function(feature, layer) {
+            // feature -- stores the information about the feature
+            // layer is the line or shape that is being drawn onto the map
+            // console.log(feature.properties) // do this to inspect the feature properties of your geojson file
+            layer.bindPopup(feature.properties.Description);
+        }
+    }).addTo(mapObject); // this is also a layer that can go into the base layer or overlays
+    
+    // style the layer --> check documentation for more customisation options
+    cyclingLayer.setStyle({
+        'color': 'red'
+    })
+    
+    return cyclingLayer;
+};
+
+// LOAD IN THE NPARKS DATA
+async function loadNParks(){
+    let response = await axios.get('data/nparks.geojson');
+    let nparks = L.geoJson(response.data, {
+        onEachFeature:function(feature, layer) {
+            layer.bindPopup(feature.properties.Description);
+        }
+    }).addTo(mapObject); // this is also a layer that can go into the base layer or overlays
+    
+     // style the layer
+     nparks.setStyle({
+        'color': 'green'
+    })
+    
+    return nparks;
+};
+
+window.addEventListener('DOMContentLoaded', function(){
+    loadCyclingPath();
+    loadNParks(); 
+});
